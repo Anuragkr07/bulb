cf90f24e50794266b0ee126cd14ec5b9245f9907
Anurag Kumar Singh <86841935+Anuragkr07@users.noreply.github.com>
2022-03-14 17:30:15 +0530

    Delete bulb directory

--- bulb/script.js
+++
@@ -1,9 +0,0 @@
-function bulbOn(){
-    document.querySelector(".bulboff").setAttribute("src","img/bulbon.gif");
-    
-}
-
-function bulbOff(){
-    document.querySelector(".bulboff").setAttribute("src","img/bulboff.gif");
-    
-}
